c59a2d32fb61126d886c50000e69c22e276bbaa9
Kagiso Monageng <kagisoboasmonageng@gmail.com>
2026-02-20 12:15:00 +0200

    redo

--- src/modules/services/services.schema.js
+++
@@ -1,9 +0,0 @@
-const Joi = require('joi');
-
-const createServiceSchema = Joi.object({
-  name: Joi.string().min(2).max(100).required(),
-  description: Joi.string().allow('', null),
-  icon: Joi.string().allow('', null)
-});
-
-module.exports = { createServiceSchema };
--- src/modules/users/users.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { authenticate } = require('../../middleware/auth');
-const controller = require('./users.controller');
-
-const router = express.Router();
-
-router.get('/me', authenticate, controller.me);
-
-module.exports = router;
